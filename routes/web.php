118ecf0915da6c672b9ff4ef6dc99df02d84f128
Muhammad Diaz R <muhamaddiaz10@gmail.com>
2018-04-23 10:18:18 -0700

    Menambahkan authentikasi

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', ['uses' => 'StaticPagesController@index', 'as' => 'staticpages.index']);
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
